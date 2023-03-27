00cd3685e8ab6091d0cbbddf505eec2226364c32
myelhome <rgty2000@mail.ru>
2023-03-27 18:52:42 +0400

    Remove unnesassary dependensies

--- backend/core/src/main/java/com/videototextaudio/core/VideoToTextAudioApplication.java
+++ backend/core/src/main/java/com/videototextaudio/core/VideoToTextAudioApplication.java
@@ -1,16 +1,8 @@
 package com.videototextaudio.core;
 
-import com.videototextaudio.core.repository.AudioRepositoryImpl;
-import com.videototextaudio.core.service.parser.TimestampParserImpl;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Component;
-
-import java.util.List;
 
 @SpringBootApplication
 @OpenAPIDefinition
@@ -18,35 +10,5 @@ public class VideoToTextAudioApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(VideoToTextAudioApplication.class, args);
-    }
-}
-
-@Component
-@RequiredArgsConstructor
-@Slf4j
- class CommandLineAppStartupRunner implements CommandLineRunner {
-
-    private final AudioRepositoryImpl audioRepository;
-
-    @Override
-    public void run(String...args) throws Exception {
-//        log.info("Remove");
-//
-//        long l = System.currentTimeMillis();
-//        audioRepository.removeAll("test");
-//        log.info(String.valueOf(System.currentTimeMillis() - l));
-//
-//        log.info("Add");
-//
-//        l = System.currentTimeMillis();
-//        for(int i = 0; i < 3600; i++) audioRepository.save("test", i, "sentence " + i);
-//        log.info(String.valueOf(System.currentTimeMillis() - l));
-//
-//        log.info("Read");
-//
-//        l = System.currentTimeMillis();
-//        var testString = audioRepository.getAllTimestampsString("test");
-//        log.info(testString.toString());
-//        log.info(String.valueOf(System.currentTimeMillis() - l));
     }
 }
--- backend/core/src/test/java/com/videototextaudio/core/VideoToTextAudioApplicationTests.java
+++ backend/core/src/test/java/com/videototextaudio/core/VideoToTextAudioApplicationTests.java
@@ -6,13 +6,10 @@ import com.videototextaudio.core.repository.AudioRepositoryImpl;
 import com.videototextaudio.core.repository.VideoProcessingRepositoryImpl;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 @SpringBootTest
-@RunWith(SpringRunner.class)
 public class VideoToTextAudioApplicationTests {
 
     private final VideoConvertorController controller;
