f6fba84a3053ea2b17e7f961866fc7781f6504a5
BigBey <bey.sviatoslav@yandex.ru>
2023-03-27 21:42:28 +0300

    draft

--- frontend/index.js
+++ frontend/index.js
@@ -2,6 +2,7 @@
 const express = require('express'); //Import the express dependency
 const app = express();              //Instantiate an express app, the main work horse of this server
 const port = 5000;                  //Save the port number where your server will be listening
+const {pipeline} = require('node:stream/promises');
 
 //Create media-server
 const ms = require('mediaserver');
@@ -15,24 +16,17 @@ const fs = require('fs');
 const load = require("audio-loader");
 const play = require("audio-play");
 
-//Idiomatic expression in express to route and respond to a client request
-app.get('/', (req, res) => {        //get requests to the root ("/") will route here
-    //server responds by sending the index.html file to the client's browser
-    res.sendFile('index.html', {root: __dirname});                                                 //the .sendFile method needs the absolute path to the file, see: https://expressjs.com/en/4x/api.html#res.sendFile
-});
-
-app.get('/getAudio', (req, res) => {
+var getAudio = function textToSpeech(audio) { // sample async action
     const params = new URLSearchParams();
-    const text = 'Вам посылка от кота Матроскина :) '
 
-    params.append('text', text);
+    params.append('text', audio.text);
     params.append('voice', 'zahar');
     params.append('emotion', 'good');
     params.append('lang', 'ru-RU');
     params.append('speed', '1.0');
     params.append('format', 'mp3');
 
-    fetch('https://tts.api.cloud.yandex.net/speech/v1/tts:synthesize', {
+    return fetch('https://tts.api.cloud.yandex.net/speech/v1/tts:synthesize', {
         method: 'post',
         body: params,
         headers: {
@@ -41,17 +35,50 @@ app.get('/getAudio', (req, res) => {
         },
     })
         .then(result => {
-            console.log(result);
-            res.send(JSON.stringify({created: 'success'}))
-            // return res.json();
-            const dest = fs.createWriteStream('./octocat.mp3');
-            const r = result.body.pipe(dest);
-            r.on('finish', function () {
-                const play = require('audio-play');
-                const load = require('audio-loader');
+            const dest = fs.createWriteStream('./media/' + audio.start + '.mp3');
+            return pipeline(result.body, dest);
+            // r.on('finish', function () {
+            //     const play = require('audio-play');
+            //     const load = require('audio-loader');
+            //
+            //     load('./octocat.mp3').then(play);
+            // });
+        })
+        .catch(err => console.error(err));
+};
 
-                load('./octocat.mp3').then(play);
+//Idiomatic expression in express to route and respond to a client request
+app.get('/', (req, res) => {        //get requests to the root ("/") will route here
+    //server responds by sending the index.html file to the client's browser
+    res.sendFile('index.html', {root: __dirname});                                                 //the .sendFile method needs the absolute path to the file, see: https://expressjs.com/en/4x/api.html#res.sendFile
+});
+
+app.get('/getAudios', (req, res) => {
+    //Для нашего апи
+    // const params = new URLSearchParams();
+    // params.append('url', req.query.url);
+    //
+    // fetch('api', {
+    //     method: 'get',
+    //     body: params,
+    //     headers: {
+    //         // 'Content-Type': 'application/x-www-form-urlencoded',
+    //         'Authorization': 'Api-Key ' + api_key,
+    //     },
+    // });
+
+    var timestamps = require('./timestamps.json')
+
+    Promise.all(timestamps.audios.map(getAudio))
+        .then(result => {
+            fs.readdir('./media/', (err, files) => {
+                files.forEach(file => {
+                    console.log(file);
+                });
             });
+            console.log(result);
+            //res.send(JSON.stringify({created: 'success'}))
+            res.send(JSON.stringify(timestamps.audios));
         })
         .catch(err => console.error(err));
 });
